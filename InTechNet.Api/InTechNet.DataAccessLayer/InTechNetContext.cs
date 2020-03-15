ec0f82cd73f6eb2c8158ca5c3bcec692aacde826
Raphael Clarisse <raphael.clarisse21@gmail.com>
2020-03-15 13:25:41 +0100

    ⚙️ Clean up database contexts, migrations, JSON files, methods

--- InTechNet.Api/InTechNet.DataAccessLayer/InTechNetContext.cs
+++ InTechNet.Api/InTechNet.DataAccessLayer/InTechNetContext.cs
@@ -48,39 +48,6 @@ namespace InTechNet.DataAccessLayer
 
             modelBuilder.Entity<Hub>()
                 .HasIndex(b => b.HubName);
-
-            modelBuilder.Entity<Moderator>()
-                .HasData(new Moderator
-                {
-                    IdModerator = 1,
-                    ModeratorEmail = "moderator@intechnet.io",
-                    ModeratorNickname = "moderator",
-                    // From raw: "moderator"
-                    ModeratorPassword =
-                        "720E39C10B81B3652B149FA74B3757AD1453F10FD4445F2A1AB4196BF2D23CE5D64A8DCD6DE157194853F35CC160F8A851155261B82B271BB81AD0B700AF9992",
-                    ModeratorSalt = "moderator-salt"
-                });
-
-            modelBuilder.Entity<Pupil>()
-                .HasData(new Pupil
-                {
-                    IdPupil = 1,
-                    PupilEmail = "pupil@intechnet.io",
-                    PupilNickname = "pupil",
-                    // From raw: "pupil"
-                    PupilPassword =
-                        "CF28AF1039C0348CE7715232444454F47E085D6859913BFE531008D1BEF4992D27D7A3301E9CC70004F0F42513676FC01B941C848160351D389BBC3A264DC0E2",
-                    PupilSalt = "pupil-salt"
-                });
-
-            modelBuilder.Entity<Hub>()
-                .HasData(new Hub
-                {
-                    IdHub = 1,
-                    HubLink = "hub-link",
-                    HubCreationDate = DateTime.Now,
-                    HubName = "hub-name"
-                });
         }
     }
 }
