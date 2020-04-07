cfc3c93703d740563fab5ca4a5e44f73ca624e92
Pierre Bouillon <pro.pierre.bouillon@protonmail.com>
2020-04-07 09:16:16 +0200

    🐛 fix module status toggle in service (#59)
    * 🐛 Fix module status toggle
    
    * ✨ Enlarge module description size

---
+++ InTechNet.Api/InTechNet.DataAccessLayer/Migrations/20200406215216_EnlargeModuleDescriptionSize.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InTechNet.DataAccessLayer.Migrations
+{
+    public partial class EnlargeModuleDescriptionSize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ModuleDescription",
+                schema: "public",
+                table: "module",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(128)",
+                oldMaxLength: 128,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ModuleDescription",
+                schema: "public",
+                table: "module",
+                type: "character varying(128)",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 256,
+                oldNullable: true);
+        }
+    }
+}
