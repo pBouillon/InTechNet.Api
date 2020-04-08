c5831557078de7b0aa2b8a7778c95586b6d5c5dc
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-04-08 11:10:57 +0200

    ✨ Add illegal operation on modules exception

---
+++ InTechNet.Api/InTechNet.Exception/Module/IllegalModuleOperationException.cs
@@ -0,0 +1,21 @@
+﻿namespace InTechNet.Exception.Module
+{
+    /// <summary>
+    /// Exception to be thrown when attempting to perform an operation on a module
+    /// that is not allowed in the current context
+    /// </summary>
+    public class IllegalModuleOperationException : BaseException
+    {
+        /// <summary>
+        /// Custom exception message for this exception
+        /// </summary>
+        protected const string ExceptionMessage = "The requested operation can not be performed in the current context";
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="innerException">Nullable inner-exception</param>
+        public IllegalModuleOperationException(System.Exception innerException = null)
+            : base(ExceptionMessage, innerException) { }
+    }
+}
