835f4c8482cb0b66a660be0823e39afc728051a4
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-04-08 11:10:43 +0200

    ✨ Add unknown state exception

---
+++ InTechNet.Api/InTechNet.Exception/Module/UnknownStateException.cs
@@ -0,0 +1,20 @@
+﻿namespace InTechNet.Exception.Module
+{
+    /// <summary>
+    /// Exception to be thrown when attempting to query an unknown state
+    /// </summary>
+    public class UnknownStateException : BaseException
+    {
+        /// <summary>
+        /// Custom exception message for this exception
+        /// </summary>
+        protected const string ExceptionMessage = "Unable to find this state in the given context";
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="innerException">Nullable inner-exception</param>
+        public UnknownStateException(System.Exception innerException = null)
+            : base(ExceptionMessage, innerException) { }
+    }
+}
